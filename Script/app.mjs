89b6147635d49d68d9984e52264fc17c1c4a79a4
Alfredo A. Sanger III <althirdysanger@gmail.com>
2023-05-09 00:06:54 +0800

    commit 12:06

--- Script/app.mjs
+++ Script/app.mjs
@@ -1,4 +1,4 @@
-import recommend from "./data.mjs"
+import Data from "./data.mjs"
 
 // Function to select the element in HTML
 const selector = (name) => {
@@ -8,9 +8,68 @@ const selector = (name) => {
 
 
 //variable Data
-const recommended = recommend,
-      glider = selector(".glider")
+// start of filtering
+const recommended = Data.filter(n => {
+    if (n.secondary_id == `recommended`) {
+        return n
+    }
+}),
+    glider = selector(".glider")
 
+const main_book = Data.filter(n => {
+    if (n.secondary_id == `main_books`) {
+        return n
+    }
+
+})
+// End of filtering
+const btn = document.querySelectorAll(".book_nav")
+const _showBooks = selector(".books")
+
+
+
+//show Main books
+
+// function for showing of data 
+const showin_book = (data) => {
+    const main_book_show = data.map(n => (
+        `   <div class="book_card">
+        <img src="${n.img}" alt="" class="book_img">
+        <div class="book_card_txt">
+            <h3>${n.title}</h3>
+            <p>${n.author}</p>
+        </div>
+        <div class="rating">
+            <img src="./Imges/star.png" alt="">
+            <button id='${n.id}'>view</button>
+        </div>
+    </div>
+    `
+    ))
+
+    _showBooks.innerHTML = main_book_show.join('')
+}
+
+
+const main_book_show = main_book.map(n => (
+    `   <div class="book_card">
+        <img src="${n.img}" alt="" class="book_img">
+        <div class="book_card_txt">
+            <h3>${n.title}</h3>
+            <p>${n.author}</p>
+        </div>
+        <div class="rating">
+            <img src="./Imges/star.png" alt="">
+            <button id='${n.id}'>view</button>
+        </div>
+    </div>
+    `
+))
+
+_showBooks.innerHTML = main_book_show.join('')
+// end of show main books
+
+// Recommend card 
 const recommend_data = recommended.map(n => (
     `                    <div class="card">
 <img src="${n.img}" alt="" class="book">
@@ -27,15 +86,25 @@ const recommend_data = recommended.map(n => (
 </div>`
 ))
 glider.innerHTML = recommend_data.join('')
-console.log(recommend_data)
 
-// Recommend card 
+//  end Recommend card 
 
+btn.forEach(n => {
+    n.addEventListener('click', (e) => {
+        const category = e.currentTarget.dataset.id
+        const booksitem = Data.filter(n=>{
+            if(n.category== category){
+                return n 
+            }
+        })
+        if(category == "All Genre"){
+            showin_book(Data)
+        }else{
+            showin_book(booksitem)
+        }
+    })
 
-
-
-
-
+})
 
 
 // Element variable
@@ -50,64 +119,90 @@ toggle.addEventListener('click', () => {
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Glider
 new Glider(document.querySelector('.glider'), {
     slidesToShow: 1,
     slidesToScroll: 5,
     draggable: true,
     dots: '.dots',
-    responsive : [{
+    responsive: [{
         breakpoint: 360,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 1.3,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 1.3,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
     },
     {
         breakpoint: 400,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 1.5,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
-    },{
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 1.5,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
+    }, {
         breakpoint: 1366,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 4.5,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
-    },{
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 4.5,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
+    }, {
         breakpoint: 1024,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 4,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
-    },{
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 4,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
+    }, {
         breakpoint: 768,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 2.5,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
-    },{
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2.5,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
+    }, {
         breakpoint: 600,
         settings: {
-        // Set to `auto` and provide item width to adjust to viewport
-        slidesToShow: 2,
-        slidesToScroll: 'auto',
-        itemWidth: 150,
-        duration: 0.25}
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2,
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+        }
     },
 
 
-]
+    ]
 });
 
